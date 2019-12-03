7bf5d38ee57951888eedc00061c4ed7193498681
乐潇游 <tx@vimplus.com>
2019-12-03 16:11:19 +0800

    update addBigNum

--- 09-algorithm/addBigNum.js
+++ 09-algorithm/addBigNum.js
@@ -4,46 +4,19 @@
  * @param {*} b 
  */
 function add(a, b) {
-    var arrayA = a.split("");//将字符串转为数组
-    var arrayB = b.split("");
-    var ArrayA = [];
-    var ArrayB = [];
-    var l1 = arrayA.length;
-    var l2 = arrayB.length;
-    var arrayC = [];
-
-    var n = l1 < l2 ? l1 : l2;
-
-    for (var i = 0; i < arrayA.length; i++) {//将string类型的数组转为int型
-        ArrayA[i] = parseInt(arrayA[i]);
+    var arrayA = a.split('');   // 将字符串转为数组
+    var arrayB = b.split('');
+    var res = '';
+    var temp = 0;
+    while(arrayA.length || arrayB.length || temp) {
+        temp += ~~arrayA.pop() + ~~arrayB.pop();
+        res = (temp % 10) + res;
+        temp = temp > 9;
     }
-    for (var i = 0; i < arrayB.length; i++) {
-        ArrayB[i] = parseInt(arrayB[i]);
-    }
-
-    if (ArrayA.length >= ArrayB.length) {//判断哪个数组较大
-        max = ArrayA;
-        min = ArrayB;
-    } else {
-        min = ArrayA;
-        max = ArrayB;
-    }
-
-    for (var i = n - 1; i >= 0; i--) {
-        arrayC[i] = (min[i]) + max[Math.abs(l1 - l2) + i];
-        //从个位开始相加
-        if ((arrayC[i]) > 9) {
-            //遇10进一
-            arrayC[i] -= 10;
-            max[Math.abs(l1 - l2) + i - 1] += 1;    // 进一
-        }
-    }
-    var result = [];
-    for (var i = 0; i < Math.abs(l1 - l2); i++) {//补齐高位
-        result[i] = max[i];
-
-    }
-    result = (result.concat(arrayC)).join("");//将数组连接成字符串
-    console.log(result);
-
+    return res.replace(/^0+/, '');
 }
+
+// ~~:双非按位取反运算符，相当于Math.floor()，对于正数，它向下取整；对于负数，向上取整；非数字取值为0
+
+console.log(add('123', '23'));
+console.log(add('999', '222'));
