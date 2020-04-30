180ed0f53e938a207a2300d029d97300a92bcf75
乐潇游 <tx@vimplus.com>
2020-04-30 17:48:04 +0800

    update new

--- JavaScript/New/index.js
+++ JavaScript/New/index.js
@@ -24,14 +24,3 @@ function Person(name) {
 }
 
 const p = _new(Person, 'txboy');
-
-
-// 怎么判断一个函数是否被new调用过？
-// ES6方式
-function Foo() {
-    if (!new.target) throw "Foo() must be called with new";
-    console.log("Foo instantiated with new");
-}
-
-// ES5方式
-this instanceof Foo
